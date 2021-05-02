52a9964a9c53004a2049c3ae38f25b7b195ce723
pavipixel <pavipixel@gmail.com>
2021-05-02 18:33:50 +0530

    Changes to Simple Interest Calculator

--- script.js
+++ script.js
@@ -1,6 +1,48 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
+    //Get the values entered by user
+    var principal = document.getElementById("principal").value;
+
+    if (principal == "" ) {
+        alert("Enter a Positive Number for Principal Amount");
+        return false;
+    }
+
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+
+    //Simple Interest Calculation
+    var amount = principal * years * rate / 100;
+    var year = new Date().getFullYear()+parseInt(years);
     
+    document.getElementById("result").innerHTML="If you deposit <mark>"+principal+"</mark>,\<br\>at an interest rate of <mark>"+rate+"%</mark>\<br\>You will receive an amount of <mark>"+amount+"</mark>,\<br\>in the year <mark>"+year+"</mark>\<br\>"
+    
+}
+    
+function updateRate() 
+{
+    //to show the Rate value
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval + "%";
+}
+
+function checkdata() {
+            
+    //To Validate the principal amount value
+    var principal = document.getElementById("principal");
+
+    if (principal.value == "" ) {
+        alert("Enter a Positive Number");
+        principal.focus();
+        return false;
+    }
+
+    if (principal.value <= 0 ) {
+        alert("Enter a Positive Number");
+        principal.focus();
+        return false;
+    }
+    
+    return true;
 }
         
